0af7fe37a4eca1ea3cc59af6ba79018fdabd7492
baoge972 <wangsir666998@gmail.com>
2025-10-13 04:20:37 +0800

    Site updated: 2025-10-13 04:20:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/13/模板1/"];function toRandomPost(){
+var posts=["2025/10/13/模板1/","2025/05/23/模板2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
